2aec68e6e40db6c8b56a66576598ae74e962c82b
Hafsah <hafsahaliyu29@gmail.com>
2022-09-20 03:26:23 +0100

    commit

---
+++ script.js
@@ -0,0 +1,21 @@
+const personalInfo    = document.getElementById("personalInfo")
+const educationalInfo = document.getElementById("educationalInfo")
+const healthInfo      = document.getElementById("healthInfo")
+
+
+const lName=document.querySelector("#email")
+
+
+
+
+function nextbtn1(){
+     signup.style.display="flex"
+     login.style.display="none"
+ }
+ 
+function backbtn2(){
+    signup.style.display="flex"
+    login.style.display="none"
+}
+ 
+ 
